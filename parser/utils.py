8bc2b5ec66a457da8095291cf074dd396552ec73
veronesi <veronesi@ihp-microelectronics.com>
2025-08-13 14:49:32 +0200

    replace single module update

--- parser/utils.py
+++ parser/utils.py
@@ -15,15 +15,36 @@ def replace_module(model, old_module, replacer, module_name = ''):
     
     return model  # Return the modified model copy
 
-def replace_singleModule(model, module_name, replacer):
+# def replace_singleModule(model, module_name, replacer):
+#     # Recursively replace modules in the copied model
+#     for name, module in model.named_children():
+#         if hasattr(module, module_name):
+#             # Instantiate the replacement module
+#             replacement = replacer(module, name)
+#             setattr(model, name, replacement)
+#         else:
+#             # If the module has children, apply recursively
+#             replace_singleModule(module, module_name, replace_module)
+
+#     return model  # Return the modified model copy
+
+def replace_singleModule(model, old_module_name, supported_list, replacers_list, module_name = ''):
     # Recursively replace modules in the copied model
     for name, module in model.named_children():
-        if hasattr(module, module_name):
-            # Instantiate the replacement module
-            replacement = replacer(module, name)
-            setattr(model, name, replacement)
-        else:
-            # If the module has children, apply recursively
-            replace_singleModule(module, module_name, replace_module)
-
+        full_name = f'{module_name}.{name}' if module_name != '' else name
+        if not hasattr(module, 'nvdla'):
+            # if isinstance(module, old_module):
+            if full_name == old_module_name:
+                # Instantiate the replacement module
+                for idx, supported_module in enumerate(supported_list):
+                    if isinstance(module, supported_module):
+                        replacer = replacers_list[idx]
+                        replacement = replacer(module, full_name)
+                        setattr(model, name, replacement)
+            #     replacement = replacer(module, full_name)
+            #     setattr(model, name, replacement)
+            else:
+                # If the module has children, apply recursively
+                replace_singleModule(module, old_module_name, supported_list, replacers_list, full_name)
+    
     return model  # Return the modified model copy
