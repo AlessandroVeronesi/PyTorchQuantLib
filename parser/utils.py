37452e05496cad70def695f457a2e11a43611397
veronesi <veronesi@ihp-microelectronics.com>
2025-08-26 17:29:59 +0200

    added DW bypass

--- parser/utils.py
+++ parser/utils.py
@@ -6,7 +6,11 @@ def replace_module(model, old_module, replacer, module_name = ''):
         full_name = f'{module_name}.{name}' if module_name != '' else name
         if not hasattr(module, 'nvdla'):
             if isinstance(module, old_module):
-                # Instantiate the replacement module
+                if hasattr(module, 'groups'):
+                    if (module.groups > 1):
+                        # Bypass DW layers
+                        replace_module(module, old_module, replacer, full_name)
+                # Instantiate the replacementV module
                 replacement = replacer(module, full_name)
                 setattr(model, name, replacement)
             else:
@@ -14,19 +18,6 @@ def replace_module(model, old_module, replacer, module_name = ''):
                 replace_module(module, old_module, replacer, full_name)
     
     return model  # Return the modified model copy
-
-# def replace_singleModule(model, module_name, replacer):
-#     # Recursively replace modules in the copied model
-#     for name, module in model.named_children():
-#         if hasattr(module, module_name):
-#             # Instantiate the replacement module
-#             replacement = replacer(module, name)
-#             setattr(model, name, replacement)
-#         else:
-#             # If the module has children, apply recursively
-#             replace_singleModule(module, module_name, replace_module)
-
-#     return model  # Return the modified model copy
 
 def replace_singleModule(model, old_module_name, supported_list, replacers_list, module_name = ''):
     # Recursively replace modules in the copied model
